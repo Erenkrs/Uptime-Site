612c9d9385b10e9f8abcafd21070419a6a364160
DalgaGaPlay <156920340+DalgaGaPlay@users.noreply.github.com>
2024-02-03 12:25:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,13 @@ const path = require('path')
 // ...
 
 
-
+express.json()
+express.urlencoded({ extended: true })
 const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views')); // Add this line
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public'))); // Add this line
 
 const clientId = process.env.REACT_APP_CLIENT_ID;
 const clientSecret = process.env.REACT_APP_CLIENT_SECRET;
